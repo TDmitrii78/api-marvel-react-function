d91ba0df13b899101922b565626fb0c77bdf0f00
dmitrii <hdmail2022@gmail.com>
2023-04-09 21:14:10 +0400

    my commit)

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,43 +1,27 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundaries from "../errorBoundaries/ErrorBoundaries";
-
-import ComicsList from '../comicsList/ComicsList';
-
-import decoration from '../../resources/img/vision.png';
+import MainPage from "../pages/MainPage";
+import ComicsPage from "../pages/ComicsPages";
 
 
 const App = () => {
-    const [id, setId] = useState(null);
 
-    const onClickCharacter = (id) => {
-        setId(id = id);
-    }
     return (
-        <div className="app">
-            <ErrorBoundaries>
-                <AppHeader/>
-            </ErrorBoundaries>
-            <main>
-                {/* <ComicsList/> */}
+        <Router>
+            <div className="app">
                 <ErrorBoundaries>
-                    <RandomChar/>
+                    <AppHeader/>
                 </ErrorBoundaries>
-                <div className="char__content">
-                    <ErrorBoundaries>
-                        <CharList onClickCharacter={(id) => onClickCharacter(id)}/>
-                    </ErrorBoundaries>
-                    <ErrorBoundaries>
-                        <CharInfo id={id}/>
-                    </ErrorBoundaries>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-            </main>
-        </div>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<MainPage/>}/>    
+                        <Route path="/comics" element={<ComicsPage/>}/>
+                    </Routes>
+                </main>
+            </div>
+        </Router>
     )
 }
 
---
+++ src/components/pages/MainPage.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+import ErrorBoundaries from "../errorBoundaries/ErrorBoundaries";
+import RandomChar from "../randomChar/RandomChar";
+import CharList from "../charList/CharList";
+import CharInfo from "../charInfo/CharInfo";
+
+import decoration from '../../resources/img/vision.png';
+
+const MainPage = () => {
+    const [id, setId] = useState(null);
+
+    const onClickCharacter = (id) => {
+        setId(id = id);
+    }
+
+    return (
+        <>
+            <ErrorBoundaries>
+                <RandomChar/>
+            </ErrorBoundaries>
+            <div className="char__content">
+                <ErrorBoundaries>
+                    <CharList onClickCharacter={(id) => onClickCharacter(id)}/>
+                </ErrorBoundaries>
+                <ErrorBoundaries>
+                    <CharInfo id={id}/>
+                </ErrorBoundaries>
+            </div>
+            <img className="bg-decoration" src={decoration} alt="vision"/>
+        </>
+    )
+}
+
+export default MainPage;
